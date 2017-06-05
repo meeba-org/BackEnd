9ce32bffc6542250f02457f09ce958aacd5f4c9b
chenop <chenop@gmail.com>
2017-06-05 23:04:35 +0300

    Adding CI support

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ app.set('port', process.env.PORT || 3000);
 
 app.get('/', function (req, res) {
 	res.send('meeba says: Hello World!')
-})
+});
 
 app.listen(app.get('port'), function () {
 	console.log('Example app listening on port ' + app.get('port'));
-})
+});
+
+module.exports = app;
---
+++ test/sanityTest.js
@@ -0,0 +1,14 @@
+const request = require('supertest');
+const app = require('../server');
+
+describe("Sanity tests", function () {
+	describe('GET /', function () {
+		it('respond with 200', function (done) {
+			request(app)
+				.get('/')
+				.expect('Content-Type', /text\/html/)
+				.expect(200, done);
+		});
+	});
+});
+
