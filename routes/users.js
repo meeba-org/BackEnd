1ecf36a597663888f88b98330eeea3ed6eca8022
chenop <chenop@gmail.com>
2017-07-07 10:21:27 +0300

    Tiny refactor in login

--- routes/users.js
+++ routes/users.js
@@ -12,47 +12,47 @@ const User = require('../models/user');
 
 
 router.post('/register', (req, res) => {
-  const uid = req.body.uid;
-  const first_name = req.body.first_name;
-  const last_name = req.body.last_name;
-  const email = req.body.email;
-  const password = req.body.password;
-  const password2 = req.body.password2;
-  const role = req.body.role;
+    const uid = req.body.uid;
+    const first_name = req.body.first_name;
+    const last_name = req.body.last_name;
+    const email = req.body.email;
+    const password = req.body.password;
+    const password2 = req.body.password2;
+    const role = req.body.role;
 
-  // Validation
-  req.checkBody('uid', 'id is required').notEmpty();
-  req.checkBody('first_name', 'First name is required').notEmpty();
-  req.checkBody('last_name', 'Last name is required').notEmpty();
-  req.checkBody('role', 'Role is required').notEmpty();
-  req.checkBody('email', 'Email is required').notEmpty();
-  req.checkBody('email', 'Email is not valid').isEmail();
-  req.checkBody('password', 'Password is required').notEmpty();
-  req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+    // Validation
+    req.checkBody('uid', 'id is required').notEmpty();
+    req.checkBody('first_name', 'First name is required').notEmpty();
+    req.checkBody('last_name', 'Last name is required').notEmpty();
+    req.checkBody('role', 'Role is required').notEmpty();
+    req.checkBody('email', 'Email is required').notEmpty();
+    req.checkBody('email', 'Email is not valid').isEmail();
+    req.checkBody('password', 'Password is required').notEmpty();
+    req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
 
-  const errors = req.validationErrors();
+    const errors = req.validationErrors();
 
-  if (errors){
-    return res.status(400).json({message: errors});
-  }
-  else{
-    // Passed
-    const newUser = new User({
-      uid: uid,
-      first_name: first_name,
-      last_name: last_name,
-      email: email,
-      password: password,
-      role: role
-    });
+    if (errors) {
+        return res.status(400).json({message: errors});
+    }
+    else {
+        // Passed
+        const newUser = new User({
+            uid: uid,
+            first_name: first_name,
+            last_name: last_name,
+            email: email,
+            password: password,
+            role: role
+        });
 
-    User.create(newUser, (err, user) => {
-      if (err) throw err;
-      //console.log(user);
-    });
+        User.create(newUser, (err, user) => {
+            if (err) throw err;
+            //console.log(user);
+        });
 
-    return res.status(200).json({message: "User is registered and can now login"});
-  }
+        return res.status(200).json({message: "User is registered and can now login"});
+    }
 });
 
 // passport.js works with the concept of strategies.
@@ -65,76 +65,82 @@ jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeader();
 // The secretOrKey is the secret that our tokens will be signed with. Choose this wisely or use a private key.
 jwtOptions.secretOrKey = 'tH1S1Sag00Dk3Y';
 // Define Auth Strategy
-var strategy = new JwtStrategy(jwtOptions, function(jwt_payload, next) {
-  console.log('payload received', jwt_payload);
-  User.getByUid(jwt_payload.uid, (err, user) => {
-    if (err) throw err;
-    if (!user) {
-      return next(null, false, {message: 'Unknown User'});
-    }
-    return next(null, user);
-  });
+var strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
+    console.log('payload received', jwt_payload);
+    User.getByUid(jwt_payload.uid, (err, user) => {
+        if (err) throw err;
+        if (!user) {
+            return next(null, false, {message: 'Unknown User'});
+        }
+        return next(null, user);
+    });
 });
 
 passport.use(strategy);
 
 // Creating a /login route to acquire a token
-router.post('/login', function(req, res) {
-  if(req.body.uid && req.body.password){
-    var id = req.body.uid;
-    var password = req.body.password;
-  }
-  User.getByUid(id, (err, user) => {
-    if (err) throw err;
-    if (!user) {
-      return res.status(401).json({message:"no such user found"});
+router.post('/login', function (req, res) {
+    let uid = req.body.uid;
+    let password = req.body.password;
+
+    if (!uid || !password) {
+        return res.status(500).json("uid or password are missing")
     }
 
-    User.comparePassword(password, user.password, (err, isMatch) => {
-      if (err) throw err;
-      //TODO remove next line when bcrypt is fixed
-      isMatch = true;
-      if (!isMatch) {
-        return res.status(401).json({message:"passwords did not match"});
-      }
-      // from now on we'll identify the user by the id and the id is the only personalized value that goes into our token
-      // set the payload for the JWT
-      var payload = {uid: user.uid};
-      // use the jsonwebtoken package to create the token and respond with it
-      var token = jwt.sign(payload, jwtOptions.secretOrKey);
-      return res.status(200).json(token);
-    });
-  });
+    User.getByUid(id)
+        .then((user) => {
+            if (!user) {
+                return res.status(401).json({message: "no such user found"});
+            }
+
+            User.comparePassword(password, user.password, (err, isMatch) => {
+                if (err) throw err;
+                //TODO remove next line when bcrypt is fixed
+                isMatch = true;
+                if (!isMatch) {
+                    return res.status(401).json({message: "passwords did not match"});
+                }
+                // from now on we'll identify the user by the id and the id is the only personalized value that goes into our token
+                // set the payload for the JWT
+                var payload = {uid: user.uid};
+                // use the jsonwebtoken package to create the token and respond with it
+                var token = jwt.sign(payload, jwtOptions.secretOrKey);
+                return res.status(200).json(token);
+            })
+        })
+        .catch((err) => {
+            return res.status(500).json(err);
+        });
 });
 
 // Creating a /secret route, that only is available to logged in users with a JSON web token
 // The passport.authenticate part means that we pass the request through our previously defined authentication strategy and run it.
 // If it’s successful, we respond with the secret message, else the request will be unauthorized (401).
-router.get('/secret', passport.authenticate('jwt', { session: false }), (req, res) => {
-  res.json("Success! You can not see this without a token.");
+router.get('/secret', passport.authenticate('jwt', {session: false}), (req, res) => {
+    res.json("Success! You can not see this without a token.");
 });
 
 /*router.get('/logout', (req, res) => {
-  req.logout();
+ req.logout();
 
-  req.flash('success_msg', 'You are logged out');
+ req.flash('success_msg', 'You are logged out');
 
-  res.redirect('/api/v1/users/login');
-});*/
+ res.redirect('/api/v1/users/login');
+ });*/
 
 router.get('/', (req, res) => {
-  User.getAll()
-    .then((users) => res.json(users))
-    .catch((err) => res.status(500).json(err))
+    User.getAll()
+        .then((users) => res.json(users))
+        .catch((err) => res.status(500).json(err))
 });
 
 router.get('/:id', (req, res) => {
-  User.getByUid(req.params.id, (err, user) => {
-    if (err || !user) {
-      return res.status(404).json({message: 'User was not found'});
-    }
-    return res.status(200).json(user);
-  });
+    User.getByUid(req.params.id, (err, user) => {
+        if (err || !user) {
+            return res.status(404).json({message: 'User was not found'});
+        }
+        return res.status(200).json(user);
+    });
 });
 
 module.exports = router;
