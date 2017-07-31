7845193c663f7be6fd50b37fd32e3437cc97295d
chenop <chenop@gmail.com>
2017-08-01 00:44:47 +0300

    UI Improvements

--- src/components/Employees.js
+++ src/components/Employees.js
@@ -6,24 +6,28 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import CSSModules from "react-css-modules";
 import styles from "../styles/Employees.scss";
+import {Card} from "material-ui";
+import {CardContent, CardHeader} from "../../node_modules/material-ui/Card/index";
 
 class Employees extends Component {
     render() {
         let {employees} = this.props;
         return (
-            <div id="employees" className="font-style">
-                <div><h2 id="header">עובדים</h2></div>
+            <Card>
+                <CardHeader title="עובדים"/>
 
-                {employees && employees.map((employee) =>
-                    <div key={employee._id}>
-                        <span>תז: {employee.uid}</span>
-                        <span>שם: {employee.first_name + ' ' + employee.last_name}</span>
-                    </div>
-                )}
-                {(!employees || employees.length === 0) &&
+                <CardContent className="card-content">
+                    {employees && employees.map((employee) =>
+                        <div key={employee._id}>
+                            <span>תז: {employee.uid}</span>
+                            <span>שם: {employee.first_name + ' ' + employee.last_name}</span>
+                        </div>
+                    )}
+                    {(!employees || employees.length === 0) &&
                     <h2>No employees have been fetched</h2>
-                }
-            </div>
+                    }
+                </CardContent>
+            </Card>
         );
     }
 }
