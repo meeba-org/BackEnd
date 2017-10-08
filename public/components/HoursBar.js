0813c8dbad1540838af4e4ac2a00796034167884
chenop <chenop@gmail.com>
2017-10-08 23:44:16 +0300

    Monthly Report - progress!

--- public/components/HoursBar.js
+++ public/components/HoursBar.js
@@ -1,20 +1,41 @@
 import React from "react";
-import PropTypes from 'prop-types';
-
+// import PropTypes from 'prop-types';
+import styles from '../styles/HoursBar.scss';
+import CSSModules from "react-css-modules";
 
 class HoursBar extends React.Component {
     render() {
         let {regularHours, extra125Hours, extra150Hours} = this.props;
+        const MAX_HOURS_PER_MONTH = 200;
+
+        let total = (parseFloat(regularHours) + parseFloat(extra125Hours) + parseFloat(extra150Hours));
+        const factor = total / MAX_HOURS_PER_MONTH;
+        let regularHoursPer = Math.floor(regularHours / total * 100) * factor ;
+        let extra125HoursPer = Math.floor(extra125Hours / total * 100) * factor ;
+        let extra150HoursPer = Math.floor(extra150Hours / total * 100) * factor ;
         return (
-            <div>
+            <div className="stacked-bar-graph">
                 <div className="regular-hours">{regularHours}</div>
-                <div className="extra-125">{extra125Hours}</div>
-                <div className="extra-150">{extra150Hours}</div>
+                {extra125HoursPer > 0 &&
+                    <div className="extra-125">{extra125Hours}</div>
+                }
+                {extra150HoursPer > 0 &&
+                    <div className="extra-150">{extra150Hours}</div>
+                }
+
+                <span style={{width: regularHoursPer + '%'}} className="regular-hours-bar" />
+                {extra125HoursPer > 0 &&
+                    <span style={{width: extra125HoursPer + '%'}} className="extra-125-bar" />
+                }
+                {extra150HoursPer > 0 &&
+                    <span style={{width: extra150HoursPer + '%'}} className="extra-150-bar" />
+                }
             </div>
         );
     }
 }
 
-HoursBar.propTypes = {};
+// HoursBar.propTypes = {};
 
-export default HoursBar;
+export default CSSModules(HoursBar, styles);
+
