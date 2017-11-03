27669f07d787c2fbd7fa368f456dec663af1e136
chenop <chenop@gmail.com>
2017-11-03 22:22:56 +0200

    Adding export to excel disabled

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,18 +1,18 @@
 // For info about this file refer to webpack and webpack-hot-middleware documentation
 // For info on how we're generating bundles with hashed filenames for cache busting: https://medium.com/@okonetchnikov/long-term-caching-of-static-assets-with-webpack-1ecb139adb95#.w99i89nsz
-import webpack from 'webpack';
-import ExtractTextPlugin from 'extract-text-webpack-plugin';
-import WebpackMd5Hash from 'webpack-md5-hash';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import autoprefixer from 'autoprefixer';
-import path from 'path';
+const webpack = require('webpack');
+const ExtractTextPlugin  = require('extract-text-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const path = require('path');
 
 const GLOBALS = {
     'process.env.NODE_ENV': JSON.stringify('production'),
     __DEV__: false
 };
 
-export default {
+module.exports = {
     resolve: {
         extensions: ['*', '.js', '.jsx', '.json']
     },
@@ -77,26 +77,14 @@ export default {
     ],
     module: {
         rules: [
-            {test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader'},
-            {test: /\.eot(\?v=\d+.\d+.\d+)?$/, loader: 'url-loader?name=[name].[ext]'},
-            {
-                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: 'url-loader?limit=10000&mimetype=application/font-woff&name=[name].[ext]'
-            },
-            {
-                test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/,
-                loader: 'url-loader?limit=10000&mimetype=application/octet-stream&name=[name].[ext]'
-            },
-            {
-                test: /\.svg(\?v=\d+.\d+.\d+)?$/,
-                loader: 'url-loader?limit=10000&mimetype=image/svg+xml&name=[name].[ext]'
-            },
+            {test: /\.jsx?$/, exclude: /node_modules/, loaders: ['babel-loader']},
+            {test: /\.eot(\?v=\d+.\d+.\d+)?$/, loader: 'file-loader'},
+            {test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff'},
+            {test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream'},
+            {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml'},
             {test: /\.(jpe?g|png|gif)$/i, loader: 'file-loader?name=[name].[ext]'},
             {test: /\.ico$/, loader: 'file-loader?name=[name].[ext]'},
-            {
-                test: /(\.css|\.scss|\.sass)$/,
-                loader: ExtractTextPlugin.extract('css-loader?sourceMap!postcss-loader!sass-loader?sourceMap')
-            }
+            {test: /\.scss$/, use: [{loader: "style-loader"}, {loader: "css-loader"}, {loader: "sass-loader"}]}
         ]
     }
 };
