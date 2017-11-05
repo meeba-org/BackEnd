eedb420ee51654223aa1234a9df0de5eab576fef
chenop <chenop@gmail.com>
2017-11-05 20:03:25 +0200

    Removing Sanity test

--- test/sanityTest.js
+++ test/sanityTest.js
@@ -1,14 +1,14 @@
-const request = require('supertest');
-const app = require('../server');
-
-describe("Sanity tests", function () {
-	describe('GET /', function () {
-		it('respond with 200', function (done) {
-			request(app)
-				.get('/')
-				.expect('Content-Type', /html/)
-				.expect(200, done);
-		});
-	});
-});
-
+// const request = require('supertest');
+// const app = require('../server');
+//
+// describe("Sanity tests", function () {
+// 	describe('GET /', function () {
+// 		it.only('respond with 200', function (done) {
+// 			request(app)
+// 				.get('/')
+// 				.expect('Content-Type', /html/)
+// 				.expect(200, done);
+// 		});
+// 	});
+// });
+//
