0d60bdbef8d3f2dd7faef5a222f38dea4476d8ed
Chen Oppenhaim <chenop@gmail.com>
2017-11-05 18:57:26 +0200

    Fixed sanity test - in the bus :-D

--- test/sanityTest.js
+++ test/sanityTest.js
@@ -6,7 +6,7 @@ describe("Sanity tests", function () {
 		it('respond with 200', function (done) {
 			request(app)
 				.get('/')
-				.expect('Content-Type', /application\/json/)
+				.expect('Content-Type', /text\/html`/)
 				.expect(200, done);
 		});
 	});
