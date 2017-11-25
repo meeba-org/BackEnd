07c44a7752194897fed5d37cbaa5024c4cf7b46c
chenop <chenop@gmail.com>
2017-11-25 09:28:51 +0200

    Fixed Tests

--- test/HolidayAnalyzer.spec.js
+++ test/HolidayAnalyzer.spec.js
@@ -1,4 +1,3 @@
-const moment = require('moment');
 const EDayType = require('../models/EDayType');
 const expect = require('chai').expect;
 let {isListedHoliday, isHoliday, isHolidayEvening} = require('../managers/HolidayAnalyzer');
@@ -25,8 +24,9 @@ describe('"HolidayAnalyzer', function () {
         expect(isHolidayEvening("2017-11-25")).to.be.false;
         expect(isHoliday("2017-11-25")).to.be.true;
     });
-    it('Monday should be regular date', EDayType.HolidayEvening, function() {
-        expect(isListedHoliday("2017-11-20")).to.be.false;
+    it('Monday should be regular date', function() {
+        expect(isListedHoliday("2017-11-20", EDayType.HolidayEvening)).to.be.false;
+        expect(isListedHoliday("2017-11-20", EDayType.Holiday)).to.be.false;
         expect(isHolidayEvening("2017-11-20")).to.be.false;
         expect(isHoliday("2017-11-20")).to.be.false;
     });
