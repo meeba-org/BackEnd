7542d362ef4cd7304881264980db4e16bd68e864
chenop <chenop@gmail.com>
2017-12-29 11:36:07 +0200

    Home UI in progress

--- public/components/home/Home.js
+++ public/components/home/Home.js
@@ -22,14 +22,14 @@ class Home extends Component {
                             </Toolbar>
                         </AppBar>
                         <div id="content">
-                        <div id="middle-content">
+                            <div id="middle-content">
 
-                            <div className="title">מיבא</div>
-                            <hr />
-                            <div className="sub-title">
-                                שעון נוכחות וחישוב שכר לעובדים שעתיים
+                                <div className="title">מיבא</div>
+                                <hr/>
+                                <div className="sub-title">
+                                    שעון נוכחות וחישוב שכר לעובדים שעתיים
+                                </div>
                             </div>
-                        </div>
                         </div>
                     </div>
                 </div>
