4bf551b5a902830e57fc93f40bff4d9fae48cc39
chenop <chenop@gmail.com>
2018-02-04 23:08:59 +0200

    Drawer Look even better

--- public/components/SideBar.js
+++ public/components/SideBar.js
@@ -15,46 +15,12 @@ import {IfAnyGranted} from "react-authorization";
 import * as ERoles from "../helpers/ERoles";
 import {Divider, Drawer, IconButton} from "material-ui";
 import classNames from 'classnames';
+import CSSModulesStyles from '../styles/SideBar.scss';
+import CSSModules from "react-css-modules";
 
 const drawerWidth = 200;
 
 const styles = theme => ({
-    root: {
-        width: '100%',
-        height: 430,
-        marginTop: theme.spacing.unit * 3,
-        zIndex: 1,
-        overflow: 'hidden',
-    },
-    appFrame: {
-        position: 'relative',
-        display: 'flex',
-        width: '100%',
-        height: '100%',
-    },
-    appBar: {
-        position: 'absolute',
-        zIndex: theme.zIndex.drawer + 1,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-    },
-    appBarShift: {
-        marginLeft: drawerWidth,
-        width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    },
-    menuButton: {
-        marginLeft: 12,
-        marginRight: 36,
-    },
-    hide: {
-        display: 'none',
-    },
     drawerPaper: {
         position: 'relative',
         height: '100%',
@@ -72,29 +38,6 @@ const styles = theme => ({
             duration: theme.transitions.duration.leavingScreen,
         }),
     },
-    drawerInner: {
-        // Make the items inside not wrap when transitioning:
-        //width: "100%",
-    },
-    drawerHeader: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'flex-end',
-        padding: '0 8px',
-        ...theme.mixins.toolbar,
-    },
-    content: {
-        width: '100%',
-        flexGrow: 1,
-        backgroundColor: theme.palette.background.default,
-        padding: 24,
-        height: 'calc(100% - 56px)',
-        marginTop: 56,
-        [theme.breakpoints.up('sm')]: {
-            height: 'calc(100% - 64px)',
-            marginTop: 64,
-        },
-    },
 });
 
 class SideBar extends React.Component {
@@ -105,7 +48,7 @@ class SideBar extends React.Component {
         const {router} = this.props;
 
         this.state = {
-            items : [
+            items: [
                 {
                     text: "מצב משמרת",
                     url: "/dashboard/report/live",
@@ -122,19 +65,19 @@ class SideBar extends React.Component {
                     text: "דו\"ח יומי",
                     url: "/dashboard/report/daily",
                     allowedRoles: [ERoles.COMPANY_MANAGER],
-                    icon: <DateRange />
+                    icon: <DateRange/>
                 },
                 {
                     text: "עובדים",
                     url: "/dashboard/employees",
                     allowedRoles: [ERoles.COMPANY_MANAGER, ERoles.SHIFT_MANAGER],
-                    icon: <PermContactCalendar />
+                    icon: <PermContactCalendar/>
                 },
                 {
                     text: "פרופיל משתמש",
                     url: "/dashboard/user",
                     allowedRoles: [ERoles.COMPANY_MANAGER],
-                    icon: <SettingsApplications />
+                    icon: <SettingsApplications/>
                 },
             ],
             open: true,
@@ -148,11 +91,11 @@ class SideBar extends React.Component {
     }
 
     handleDrawerOpen = () => {
-        this.setState({ open: true });
+        this.setState({open: true});
     };
 
     handleDrawerClose = () => {
-        this.setState({ open: false });
+        this.setState({open: false});
     };
 
     updateRoute(item, index) {
@@ -167,41 +110,46 @@ class SideBar extends React.Component {
         items.forEach(item => item.selected = false); // set selected=false for all items
         items[newIndex].selected = true;
 
-        this.setState({ items });
+        this.setState({items});
     }
 
     render() {
         const {classes, theme, userRole} = this.props;
 
         return (
-            <Drawer
-                type="permanent"
-                classes={{
-                    paper: classNames(classes.drawerPaper, !this.state.open && classes.drawerPaperClose),
-                }}
-                open={this.state.open}
+            <Drawer id="sideBar"
+                    type="permanent"
+                    classes={{
+                        paper: classNames(classes.drawerPaper, !this.state.open && classes.drawerPaperClose),
+                    }}
+                    open={this.state.open}
             >
-                <div className={classes.drawerInner}>
-                    <div className={classes.drawerHeader} onClick={this.state.open ? this.handleDrawerClose : this.handleDrawerOpen}>
+                <div>
+                    <div className="drawer-header"
+                         onClick={this.state.open ? this.handleDrawerClose : this.handleDrawerOpen}>
                         <IconButton>
-                            {this.state.open ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+                            {this.state.open ? <ChevronRightIcon/> : <ChevronLeftIcon/>}
                         </IconButton>
                     </div>
-                    <Divider />
+                    <Divider/>
                     <List>
-                    {this.state.items.map((item, index) =>
-                        <IfAnyGranted  key={index} expected={item.allowedRoles} actual={[userRole]}>
-                            <ListItem button onTouchTap={() => this.updateRoute(item, index)}
-                                      disabled={item.disabled}
-                                      style={{ backgroundColor: item.selected ? theme.palette.text.divider : 'transparent', height: "24px", paddingRight: 0}}
-                            >
-                                <ListItemIcon>
-                                    {item.icon}
-                                </ListItemIcon>
-                                <ListItemText primary={item.text} style={{paddingRight: "24px"}}/>
-                            </ListItem>
-                        </IfAnyGranted>
-                    )}
+                        {this.state.items.map((item, index) =>
+                            <IfAnyGranted key={index} expected={item.allowedRoles} actual={[userRole]}>
+                                <ListItem button onTouchTap={() => this.updateRoute(item, index)}
+                                          disabled={item.disabled}
+                                          style={{
+                                              backgroundColor: item.selected ? theme.palette.text.divider : 'transparent',
+                                              height: "24px",
+                                              paddingRight: 0
+                                          }}
+                                >
+                                    <ListItemIcon>
+                                        {item.icon}
+                                    </ListItemIcon>
+                                    <ListItemText primary={item.text} style={{paddingRight: "24px"}}/>
+                                </ListItem>
+                            </IfAnyGranted>
+                        )}
                     </List>
                 </div>
             </Drawer>
@@ -216,5 +164,5 @@ SideBar.propTypes = {
     classes: PropTypes.object,
 };
 
-export default withRouter(withTheme()(withStyles(styles, { withTheme: true })(SideBar)));
+export default withRouter(withTheme()(CSSModules(withStyles(styles, {withTheme: true})(SideBar)), CSSModulesStyles));
 
