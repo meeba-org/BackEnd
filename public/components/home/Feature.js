d89152e0304fb94802b4d7ee6c1f22d941ded587
chenop <chenop@gmail.com>
2018-06-09 23:10:47 +0300

    Fixed the Dialog onClose

--- public/components/home/Feature.js
+++ public/components/home/Feature.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import PropTypes from 'prop-types';
 import CSSModules from "react-css-modules";
 import styles from "../../styles/Feature.scss";
@@ -10,34 +10,34 @@ class Feature extends Component {
     };
 
     handleClose = () => {
-        console.log("closing: ", this.props.title);
-        this.setState({ open: false });
+        this.setState({open: false});
     };
 
     handleOpen = () => {
-        this.setState({ open: true});
+        this.setState({open: true});
     };
 
     render() {
         let {icon, title, description, sideContent, dialogContent} = this.props;
 
-        //console.log("open: ", title, this.state.open);
         return (
-            <div styleName="feature" onClick={this.handleOpen}>
-                <div styleName="side1">
-                    <div styleName="feature-title-container">
-                        <div styleName="feature-icon">{icon}</div>
-                        <div styleName="feature-title">{title}</div>
+            <Fragment>
+                <div styleName="feature" onClick={this.handleOpen}>
+                    <div styleName="side1">
+                        <div styleName="feature-title-container">
+                            <div styleName="feature-icon">{icon}</div>
+                            <div styleName="feature-title">{title}</div>
+                        </div>
+                        <div styleName="feature-description">{description}</div>
                     </div>
-                    <div styleName="feature-description">{description}</div>
-                </div>
-                <div styleName="side2">
-                    <div>{sideContent}</div>
+                    <div styleName="side2">
+                        <div>{sideContent}</div>
+                    </div>
                 </div>
                 <Dialog maxWidth={false} onClose={this.handleClose} open={this.state.open}>
                     {dialogContent}
                 </Dialog>
-            </div>
+            </Fragment>
         );
     }
 }
