5daf1dc95f0d0cc23d79b536f761f319cd23a8b3
chenop <chenop@gmail.com>
2018-07-03 10:21:55 +0300

    Added like button

--- public/components/home/Footer.js
+++ public/components/home/Footer.js
@@ -19,7 +19,9 @@ class Footer extends Component {
                     <div styleName="signature">
                         <h5>©2017 - נוצר ע"י <a target="_blank" href="https://www.linkedin.com/in/chenop/">
                             חן אופנהיים
-                        </a></h5>
+                        </a>
+                            <div className="fb-like" styleName="like" data-href="https://www.meeba.org.il" data-layout="button_count" data-action="like" data-size="large" data-show-faces="false" data-share="false"></div>
+                        </h5>
                     </div>
                 </div>
             </div>
