e466d5c9abdf784cb892b6d8163a92774786e46a
chenop <chenop@gmail.com>
2018-07-07 16:25:26 +0300

    small fixes

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -21,10 +21,9 @@ module.exports = {
     resolve: {
         extensions: ['*', '.js', '.jsx', '.json']
     },
-    devtool: false, // more info:https://webpack.github.io/docs/build-performance.html#sourcemaps and https://webpack.github.io/docs/configuration.html#devtool
+    devtool: "eval-source-map", // more info:https://webpack.github.io/docs/build-performance.html#sourcemaps and https://webpack.github.io/docs/configuration.html#devtool
     entry: path.resolve(__dirname, 'public/index'),
-    mode: "development",
-    devtool: "eval-source-map",
+    mode: "production",
     target: 'web', // necessary per https://webpack.github.io/docs/testing.html#compile-and-test
     output: {
         path: path.resolve(__dirname, 'dist'),
