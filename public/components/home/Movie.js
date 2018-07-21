bda32052b34584ca63cf97900e83637f8aaed2b3
chenop <chenop@gmail.com>
2018-07-21 10:25:23 +0300

    Fixing hot-reload, added youtube movie to home

---
+++ public/components/home/Movie.js
@@ -0,0 +1,24 @@
+import React, {Component, Fragment} from 'react';
+import CSSModules from "react-css-modules";
+import styles from "../../styles/Movie.scss";
+import YouTube from "react-youtube";
+
+class Movie extends Component {
+    state = {};
+
+    render() {
+        return (
+            <div styleName="movie-container">
+                <div styleName="title">קצת על מיבא...</div>
+                <div styleName="movie-wrapper">
+                    <YouTube videoId="oCKVp53Nefo"/>
+                </div>
+            </div>
+        );
+    }
+}
+
+Movie.propTypes = {};
+Movie.defaultProps = {};
+
+export default CSSModules(Movie, styles);
