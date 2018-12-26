88f1298ce58441e41a74930b3546e6109bbefff7
Chen.Oppenhaim <12Meetup3>
2018-12-26 15:02:20 +0200

    Location - ohhhh looks good!

--- public/components/MbGoogleMap.js
+++ public/components/MbGoogleMap.js
@@ -6,7 +6,7 @@ const API_KEY = "AIzaSyAgOEl4xMnzBoHSR9CQndSMRafzL3_EeEE";
 
 const MbGoogleMap = compose(
     withProps({
-            googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&v=3.exp&libraries=geometry,drawing,places&language=iw`,
+        googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&v=3.exp&libraries=geometry,drawing,places&language=iw`,
         loadingElement: <div style={{ height: `100%` }} />,
         containerElement: <div style={{ height: `400px` }} />,
         mapElement: <div style={{ height: `100%` }} />,
