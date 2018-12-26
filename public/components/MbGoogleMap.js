2ac9d3bb7d712b5aafa8f8c11906d2524cc54cef
Chen.Oppenhaim <12Meetup3>
2018-12-26 15:01:54 +0200

    Location - ohhhh looks good!

--- public/components/MbGoogleMap.js
+++ public/components/MbGoogleMap.js
@@ -6,7 +6,7 @@ const API_KEY = "AIzaSyAgOEl4xMnzBoHSR9CQndSMRafzL3_EeEE";
 
 const MbGoogleMap = compose(
     withProps({
-        googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&v=3.exp&libraries=geometry,drawing,places`,
+            googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&v=3.exp&libraries=geometry,drawing,places&language=iw`,
         loadingElement: <div style={{ height: `100%` }} />,
         containerElement: <div style={{ height: `400px` }} />,
         mapElement: <div style={{ height: `100%` }} />,
@@ -15,7 +15,7 @@ const MbGoogleMap = compose(
     withGoogleMap
 )((props) =>
     (<GoogleMap
-        defaultZoom={8}
+        defaultZoom={17}
         defaultCenter={{ lat: Number(props.location.latitude), lng: Number(props.location.longitude) }}
     >
         <Marker position={{ lat: Number(props.location.latitude), lng: Number(props.location.longitude) }}  />
