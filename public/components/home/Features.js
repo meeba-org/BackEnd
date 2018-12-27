6f3a9effa1603750922bfae397d84fe1b48f5263
chenop <chenop@gmail.com>
2018-12-27 16:16:41 +0200

    Support location in mobile manager for liveShift

--- public/components/home/Features.js
+++ public/components/home/Features.js
@@ -22,14 +22,15 @@ class Features extends Component {
             {
                 icon: <PhonelinkRing style={iconStyle} />,
                 title: "העובד",
-                description: <span>רישום שעות באמצעות הנייד - תמיכה <a target="_blank" href="https://play.google.com/store/apps/details?id=chenop.meeba&hl=en">באנדרואיד</a> ו<a target="_blank" href="https://itunes.apple.com/il/app/%D7%9E%D7%99%D7%91%D7%90/id1329551700?mt=8">אייפון</a></span>,
+                // description: <span>רישום שעות באמצעות הנייד - תמיכה <a target="_blank" href="https://play.google.com/store/apps/details?id=chenop.meeba&hl=en">באנדרואיד</a> ו<a target="_blank" href="https://itunes.apple.com/il/app/%D7%9E%D7%99%D7%91%D7%90/id1329551700?mt=8">אייפון</a></span>,
+                description: <span>רישום שעות באמצעות הנייד - תמיכה <a target="_blank" href="https://play.google.com/store/apps/details?id=chenop.meeba&hl=en">באנדרואיד</a></span>,
                 sideContent: <EmployeeFeatureContent/>,
                 dialogContent: <MobileImageContent />
             },
             {
                 icon: <Computer style={iconStyle} />,
                 title: "המנהל",
-                description: "מעקב אחר שעות, חישוב שעות נוספות, סטטיסטיקות",
+                description: "מעקב אחר שעות, מיקום העובד, חישוב שעות נוספות, סטטיסטיקות",
                 sideContent: <ManagerFeatureContent />,
                 dialogContent: <ManagerFeatureContent />
             },
