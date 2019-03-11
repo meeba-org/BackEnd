24d036ace30958ef4ae271c22c1cac9f0a9ca2e7
chenop <chenop@gmail.com>
2019-03-11 07:28:40 +0200

    Added monthMap

--- public/components/MonthPicker.js
+++ public/components/MonthPicker.js
@@ -12,6 +12,22 @@ class MonthPicker extends Component {
         currentYear: moment().year(),
     };
 
+    monthMap = {
+        1: "ינואר",
+        2: "פברואר",
+        3: "מרץ",
+        4: "אפריל",
+        5: "מאי",
+        6: "יוני",
+        7: "יולי",
+        8: "אוגוסט",
+        9: "ספטמבר",
+        10: "אוקטובר",
+        11: "נובמבר",
+        12: "דצמבר",
+
+    };
+
     createMonthMenuItems = () => {
         let res = [];
         const {currentYear, currentMonth} = this.state;
@@ -21,7 +37,7 @@ class MonthPicker extends Component {
         let maxMonth = currentYear === selectedYear ? currentMonth : 12;
 
         for (let i = 1; i <= maxMonth; i++) {
-            res.push(<MenuItem key={i} value={i}>{i}</MenuItem>);
+            res.push(<MenuItem key={i} value={i}>{this.monthMap[i]}</MenuItem>);
         }
 
         return res;
