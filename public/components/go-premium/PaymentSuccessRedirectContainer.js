62712f5cf2d19488bb67b69d86db33f57e584451
chen.oppenhaim <12Meetup3>
2019-07-27 11:26:53 +0300

    Improving success payment container

--- public/components/go-premium/PaymentSuccessRedirectContainer.js
+++ public/components/go-premium/PaymentSuccessRedirectContainer.js
@@ -4,13 +4,14 @@ class PaymentSuccessRedirectContainer extends Component {
 
     componentDidMount() {
         let msg = 'user_payed';
-        console.log(msg);
+        console.log(this.props);
         top.postMessage(msg, "*");
     }
-    
+
     render() {
+        console.log(this.props)
         return (
-            <h1>Payment Success!</h1>
+            <h1>Payment Success2!</h1>
         );
     }
 }
