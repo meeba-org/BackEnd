1b134c6f173693fb3385908822657cc9365bf8ec
chen.oppenhaim <12Meetup3>
2019-07-27 11:51:11 +0300

    Sending now an object

--- public/components/go-premium/PaymentSuccessRedirectContainer.js
+++ public/components/go-premium/PaymentSuccessRedirectContainer.js
@@ -3,8 +3,12 @@ import React, {Component} from 'react';
 class PaymentSuccessRedirectContainer extends Component {
 
     componentDidMount() {
-        let msg = 'user_payed';
-        console.log(this.props);
+        const {location} = this.props;
+        const token = location.query.Token;
+        const msg = {
+            token,
+            msg: 'user_payed'
+        };
         top.postMessage(msg, "*");
     }
 
