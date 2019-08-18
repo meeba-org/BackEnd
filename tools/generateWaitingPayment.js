63a046885cfaaf427d73e543d37624380e107bf2
chen.oppenhaim <12Meetup3>
2019-08-18 09:17:16 +0300

    added generateWaitingPayment script

---
+++ tools/generateWaitingPayment.js
@@ -0,0 +1,46 @@
+process.env.NODE_ENV = 'production';
+
+const CompanyModel = require("../models/CompanyModel");
+const UserModel = require("../models/UserModel");
+const mongoose = require('mongoose');
+const config = require('../config');
+const iCreditManager = require("../managers/iCreditManager");
+
+
+mongoose.connect(config.dbUrl, {useNewUrlParser: true }, () => {
+    console.log("Connected to DB successfully");
+});
+
+let companyId = "5a1a9d1e9723c88e24ef4907";
+
+const generateWaitingPayment = async (companyId) => {
+    let company = await CompanyModel.getByCompanyId(companyId);
+    if (!company)
+        throw new Error(`Company id ${companyId} was not found`);
+
+    if (!company.paymentData) {
+        throw new Error(`No Payment daya for company id ${companyId}`);
+    }
+
+    let {email} = company;
+    let {creditCardToken} = company.paymentData;
+    creditCardToken = "0a1b8304-36e6-4a11-bdc7-b43c92fc2a25"; // TODO  Test CCToken!!! - debug only - remove it in production
+
+    if (!email || !creditCardToken)
+        throw new Error(`email or creditCardToken is missing for company id ${companyId}`);
+
+    const result = await iCreditManager.generateWaitingPayment(creditCardToken, email);
+    console.log(JSON.stringify(result));
+};
+
+const run = async () => {
+    try {
+        await generateWaitingPayment(companyId);
+    } catch (e) {
+        console.error(e);
+    } finally {
+        process.exit();
+    }
+};
+
+run();
