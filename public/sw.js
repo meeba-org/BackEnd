37fb855e076ab7ea1840f21bf38d732c1a2320e5
Chen Oppenhaim <chen.oppenhaim@toluna.com>
2020-01-03 18:06:03 +0200

    PWD does not prompt to install...

--- public/sw.js
+++ public/sw.js
@@ -2,3 +2,16 @@
 self.addEventListener('fetch', function(event) {
     /** An empty fetch handler! */
 });
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    console.log("beforeinstallprompt fired", e);
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    deferredPrompt.prompt();
+});
+
+window.addEventListener('appinstalled', (evt) => {
+    console.log("appinstalled fired", evt);
+});
