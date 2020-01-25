439099dc9ecc4a6082aa047800fdab304d51da18
Chen Oppenhaim <chen.oppenhaim@toluna.com>
2020-01-25 15:59:07 +0200

    Fixed ThemeProvider

--- public/Root.js
+++ public/Root.js
@@ -1,5 +1,5 @@
 import {createMuiTheme} from "@material-ui/core";
-import ThemeProvider from "@material-ui/styles/ThemeProvider";
+import { ThemeProvider } from '@material-ui/core/styles';
 import React from 'react';
 import {Provider} from "react-redux";
 import {browserHistory, Router} from "react-router";
