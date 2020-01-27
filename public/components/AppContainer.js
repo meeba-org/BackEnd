3d3752e4a259fb1c1ccfa6d24682b033f8d0884a
Chen Oppenhaim <chen.oppenhaim@toluna.com>
2020-01-27 06:16:25 +0200

    Progress

--- public/components/AppContainer.js
+++ public/components/AppContainer.js
@@ -7,10 +7,12 @@ import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
 import {handleResize} from "../actions";
 import {fetchMetaData} from "../actions/generalActions";
 import "../styles/App.scss";
+import Dashboard from "./Dashboard";
 import FAQContainer from "./faq/FAQContainer";
 import PaymentSuccessRedirectContainer from "./go-premium/PaymentSuccessRedirectContainer";
 import Home from "./home/Home";
 import ModalRoot from "./modals/ModalRoot";
+import PrivateRoute from "./PrivateRoute";
 
 class AppContainer extends React.Component {
     componentDidMount() {
@@ -35,6 +37,7 @@ class AppContainer extends React.Component {
                 <BrowserRouter>
                     <Switch>
                         <Route path="/home" component={Home} />
+                        <PrivateRoute path="/dashboard" component={Dashboard} />
                         <Route path="/faq/:name?" component={FAQContainer} />
                         <Route path="/paymentSuccess" component={PaymentSuccessRedirectContainer} />
                         <Redirect from="/" to="/home" />
---
+++ public/components/PrivateRoute.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Redirect, Route} from "react-router";
+
+const PrivateRoute = ({ component: Component, ...rest }) => <Route {...rest} render={(props) => {
+    const token = localStorage.getItem('jwtToken');
+    return !!token ? <Component {...props} />
+        : <Redirect to='/home'/>;
+}}/>;
+
+export default PrivateRoute;
