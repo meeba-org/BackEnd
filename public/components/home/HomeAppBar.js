216f127d3b911f3265fe062d39dd6acfd4f77e75
Chen Oppenhaim <chen.oppenhaim@toluna.com>
2020-02-04 10:20:20 +0200

    Improving app bar

--- public/components/home/HomeAppBar.js
+++ public/components/home/HomeAppBar.js
@@ -1,72 +1,59 @@
 import AppBar from "@material-ui/core/AppBar";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
-import withStyles from "@material-ui/core/styles/withStyles";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import React, {Component} from "react";
 import {connect} from 'react-redux';
-import {withRouter} from "react-router-dom";
-import {Link} from "react-router-dom";
+import {Link, withRouter} from "react-router-dom";
 import {showLoginRegisterDialog} from "../../actions";
 import * as selectors from "../../selectors";
+import "../../styles/HomeAppBar.scss";
 import facebookImage from "../../styles/images/facebook.png";
 import {Logo} from "../../styles/Logo";
 
-const styles = {
-    colorPrimary: {
-        backgroundColor: "transparent",
-        boxShadow: "none",
-        color: "white"
-    },
-    rightButtonsGroup: {
-        flexGrow: 1,
-    },
-    link: {
-        color: "inherit",
-        textDecoration: "none"
-    },
-    faq: {
-        color: "white",
-        textDecoration: "none",
-        minWidth: "30px"
-    },
-    // button: {
-    //     color: "inherit",
-    //     textDecoration: "none"
-    // },
-    facebookLogo: {
-        color: "inherit",
-        minWidth: "30px",
-        paddingRight: "0px",
-        paddingLeft: "0px"
-    }
-};
-
 class HomeAppBar extends Component {
+
+    state = {
+        isTop: true
+    };
+
+    componentDidMount() {
+        document.addEventListener('scroll', this.topScrollListener);
+    }
+
+    topScrollListener = () => {
+        const isTop = window.scrollY < 100;
+        if (isTop !== this.state.isTop) {
+            this.setState({isTop});
+        }
+    };
+
     render() {
-        let {showLoginRegisterDialog, classes, location} = this.props;
+        let {showLoginRegisterDialog, location} = this.props;
+        const {isTop} = this.state;
+        const isTopClass = isTop ? 'isTop' : '';
 
         return (
-            <AppBar position="fixed" classes={{ colorPrimary: classes.colorPrimary}}>
-                <Toolbar>
-                    <Link to="/home" className={classes.link}>
-                        <IconButton aria-label="Menu" color="inherit">
-                            <Logo />
-                            <Typography type="title" color="inherit" style={{paddingRight: "10px"}}>
-                                מיבא
-                            </Typography>
-                        </IconButton>
-                    </Link>
-                    <div className={classes.rightButtonsGroup}>
+            <AppBar position="fixed" styleName={`home-app-bar ${isTopClass}`}>
+                <Toolbar styleName="toolbar">
+                    <div styleName="right-buttons-group">
+                        <Link to="/home" styleName="link">
+                            <IconButton aria-label="Menu" color="inherit">
+                                <Logo />
+                                <Typography type="title" color="inherit" style={{paddingRight: "10px"}}>
+                                    מיבא
+                                </Typography>
+                            </IconButton>
+                        </Link>
                         <Button color="inherit" href={"https://m.me/meebaOnFace"}>צ'אט</Button>
-                        <Button href="https://www.facebook.com/meebaOnFace/" className={classes.facebookLogo} target="_blank">
+                        <Button href="https://www.facebook.com/meebaOnFace/" styleName="facebook-logo" target="_blank">
                             <img src={facebookImage}/>
                         </Button>
                     </div>
                     <div>
                         {location.pathname !== '/faq' &&
-                        <Button color="inherit"><Link to="/faq" className={classes.faq}>שאלות ותשובות</Link></Button>
+                        <Button color="inherit"><Link to="/faq" styleName="faq">שאלות ותשובות</Link></Button>
                         }
                         {location.pathname === '/faq' &&
                         <Button color="inherit" onClick={showLoginRegisterDialog}>כניסה</Button>
@@ -90,4 +77,4 @@ const mapDispatchToProps = (dispatch) => {
     };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(withRouter(HomeAppBar)));
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(HomeAppBar));
