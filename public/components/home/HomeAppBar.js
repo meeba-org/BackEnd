dba0dae82ca66492ac67006776a57a6a9a6bc4fa
Chen Oppenhaim <chen.oppenhaim@toluna.com>
2020-02-04 15:19:39 +0200

    Fixed homeAppBar with strange fix :( - moving on

--- public/components/home/HomeAppBar.js
+++ public/components/home/HomeAppBar.js
@@ -3,6 +3,7 @@ import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import withStyles from '@material-ui/core/styles/withStyles';
 import React, {Component} from "react";
 import {connect} from 'react-redux';
 import {Link, withRouter} from "react-router-dom";
@@ -65,16 +66,13 @@ class HomeAppBar extends Component {
     }
 }
 
-const mapStateToProps = (state) => {
-    return {
-        isDesktop: selectors.isDesktop(state)
-    };
+const mapStateToProps = (state) => ({
+    isDesktop: selectors.isDesktop(state)
+});
+
+const mapDispatchToProps = {
+    showLoginRegisterDialog,
 };
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        showLoginRegisterDialog: () => {dispatch(showLoginRegisterDialog());},
-    };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(withRouter(HomeAppBar));
+// withStyles() is needed here for the component to not crash in production - not sure why... :(
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles()(withRouter(HomeAppBar)));
