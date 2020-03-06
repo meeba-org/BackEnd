0ceb392334d79d68fa66768b8ff0bb495d7e7bd6
Chen Oppenhaim <chen.oppenhaim@toluna.com>
2020-03-06 16:35:03 +0200

    Add slide up effect

--- public/components/home/Header.js
+++ public/components/home/Header.js
@@ -30,25 +30,28 @@ class Header extends Component {
                             <div styleName="sub-title">
                                 שעון נוכחות ומחשבון שכר
                             </div>
-                            <Button styleName="login-button" variant="contained" color="primary" onClick={showLoginRegisterDialog}>
-                                <span>כניסה</span>
-                                <ArrowBack style={{"paddingTop": "5px"}}/>
-                            </Button>
+                            <div styleName="button-badges">
+                                <Button styleName="login-button" variant="contained" color="primary"
+                                        onClick={showLoginRegisterDialog}>
+                                    <span>כניסה</span>
+                                    <ArrowBack style={{"paddingTop": "5px"}}/>
+                                </Button>
 
-                            <div styleName="badges">
-                                <div styleName="badge">
-                                    <a target="_blank"
-                                       href="https://play.google.com/store/apps/details?id=chenop.meeba&hl=en&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1">
-                                        <img alt=" Google Play כעת ב-"
-                                             src={playStoreImage}/>
-                                    </a>
-                                </div>
-                                <div styleName="badge">
-                                    <a target="_blank"
-                                       href="https://itunes.apple.com/il/app/%D7%9E%D7%99%D7%91%D7%90/id1329551700?mt=8">
-                                        <img alt=" Apple Store כעת ב-"
-                                             src={appStoreImage}/>
-                                    </a>
+                                <div styleName="badges">
+                                    <div styleName="badge">
+                                        <a target="_blank"
+                                           href="https://play.google.com/store/apps/details?id=chenop.meeba&hl=en&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1">
+                                            <img alt=" Google Play כעת ב-"
+                                                 src={playStoreImage}/>
+                                        </a>
+                                    </div>
+                                    <div styleName="badge">
+                                        <a target="_blank"
+                                           href="https://itunes.apple.com/il/app/%D7%9E%D7%99%D7%91%D7%90/id1329551700?mt=8">
+                                            <img alt=" Apple Store כעת ב-"
+                                                 src={appStoreImage}/>
+                                        </a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
