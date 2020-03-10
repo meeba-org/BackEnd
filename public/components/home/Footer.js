ae45f4703d29696315b0802dd2dfe8fa3d2e02cc
Chen Oppenhaim <chen.oppenhaim@toluna.com>
2020-03-10 21:49:36 +0200

    Removing my name

--- public/components/home/Footer.js
+++ public/components/home/Footer.js
@@ -16,9 +16,9 @@ class Footer extends Component {
                             <div className="fb-like" styleName="like" data-href="https://www.facebook.com/meebaOnFace" data-layout="button_count" data-action="like" data-size="large" data-show-faces="false" data-share="false" />
                         </Button>
                     </div>
-                    <div styleName="signature">
-                        <div>נוצר ע"י <a target="_blank" href="https://www.linkedin.com/in/chenop/">חן אופנהיים</a>, טלפון: 052-8421440</div>
-                    </div>
+                    {/*<div styleName="signature">*/}
+                    {/*    <div>נוצר ע"י <a target="_blank" href="https://www.linkedin.com/in/chenop/">חן אופנהיים</a>, טלפון: 052-8421440</div>*/}
+                    {/*</div>*/}
                     <div styleName="term-usage"><a target="_blank" href="https://docs.google.com/document/d/1vGWUvLTo2izTOBTmJBpjkKPwm4bpRkVGV9IsyJSxVzw/edit?usp=sharing">תנאי שימוש</a></div>
                 </div>
             </div>
