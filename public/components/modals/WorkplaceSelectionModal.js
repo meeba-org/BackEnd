de8862b20eb726b082a9a7267bd277c2bafcac65
Chen Oppenhaim <chen.oppenhaim@toluna.com>
2020-05-16 19:38:31 +0300

    Switch to new react maps

--- public/components/modals/WorkplaceSelectionModal.js
+++ public/components/modals/WorkplaceSelectionModal.js
@@ -1,16 +1,14 @@
-import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import {makeStyles} from '@material-ui/core/styles';
-import Typography from "@material-ui/core/Typography";
 import React, {useState} from 'react';
 import {useDispatch, useSelector} from "react-redux";
-import {hideModal, hideYesNoModal, showGoPremiumModal} from "../../actions";
+import {hideModal} from "../../actions";
 import {isDesktop} from "../../selectors";
-import MbGoogleMap from "../MbGoogleMap";
-import Address from "../workplace/Address";
+import MbGoogleMap2 from "../MbGoogleMap2";
+import PlacesAutocomplete from "../workplace/PlacesAutocomplete";
 import {EModalType} from "./EModalType";
 
 const useStyles = makeStyles({
@@ -45,14 +43,15 @@ const WorkplaceSelectionModal = ({wp = {}, open, onSave, onClose1}) => {
     //     },
     //     radius: 100
     // };
-    
+
+    const handleWorkspaceLocationChange = (location) => setWorkplace({...workplace, location});
     const onClose = () => dispatch(hideModal(EModalType.WORKPLACE_SELECTION));
 
     return (
         <Dialog onClose={onClose} open={open} classes={{paper: classes.dialogContentRoot}}>
-            <DialogContent >
-                <Address />
-                <MbGoogleMap location={workplace.location}/>
+            <DialogContent>
+                <PlacesAutocomplete location={workplace.location} onClick={handleWorkspaceLocationChange}/>
+                <MbGoogleMap2 location={workplace.location} onClick={handleWorkspaceLocationChange}/>
             </DialogContent>
             <DialogActions classes={{root: classes.dialogActions}}>
                 <Button variant="contained" onClick={onSave} color="primary" autoFocus>שמור</Button>
