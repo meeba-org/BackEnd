6f315f9731477ead82ead19525d8752f3e44837a
Chen Oppenhaim <chen.oppenhaim@toluna.com>
2020-05-21 20:48:40 +0300

    Improving UI

--- public/components/user/AllowedWorkplaces.js
+++ public/components/user/AllowedWorkplaces.js
@@ -2,15 +2,20 @@ import React from 'react';
 import Link from '@material-ui/core/Link';
 import MbCard from "../MbCard";
 import Workplace from "./Workplace";
+import "../../styles/AllowedWorkplaces.scss";
 
 const AllowedWorkplaces = ({workplaces, onAdd, onDelete, onUpdate}) => {
     return (
         <MbCard title="מקומות עבודה">
-            <Link onClick={onAdd}>הוסף</Link>
             {workplaces && workplaces.map((workplace, index) =>
-                <Workplace key={index} workplace={workplace} onDelete={() => onDelete(workplace)}
-                           onUpdate={() => onUpdate(workplace)}/>
+                (<Workplace
+                    key={index}
+                    workplace={workplace}
+                    onDelete={() => onDelete(workplace)}
+                    onUpdate={() => onUpdate(workplace)}
+                />)
             )}
+            <Link styleName="add" onClick={onAdd}>הוסף</Link>
         </MbCard>
     );
 };
--- public/components/user/Workplace.js
+++ public/components/user/Workplace.js
@@ -1,14 +1,28 @@
-import {Button} from "@material-ui/core";
-import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import IconButton from "@material-ui/core/IconButton";
 import Link from "@material-ui/core/Link";
-import React from 'react';
+import Tooltip from "@material-ui/core/Tooltip";
+import Delete from "@material-ui/icons/Delete";
+import React, {useState} from 'react';
+import "../../styles/Workplace.scss";
 
 const Workplace = ({workplace, onDelete, onUpdate}) => {
+    const [hover, setHover] = useState(false);
+
     return (
-        <Box flexDirection={"row"}>
-            <Link onClick={onUpdate}>{workplace.name}</Link>
-            <Button onClick={onDelete}>Delete</Button>
-        </Box>
+        <Grid styleName="container" container spacing={2} onMouseEnter={() => setHover(true)}
+              onMouseLeave={() => setHover(false)}>
+            <Grid item xs={3} styleName="name">
+                <Link onClick={onUpdate}>{workplace.name}</Link>
+            </Grid>
+            <Grid item xs={2}>
+                {hover &&
+                <Tooltip title="מחיקה" placement="top">
+                    <IconButton styleName='icon' onClick={onDelete}><Delete fontSize="small"/></IconButton>
+                </Tooltip>
+                }
+            </Grid>
+        </Grid>
     );
 };
 
