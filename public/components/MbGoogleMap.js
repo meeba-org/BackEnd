2a2e451a8a6dd6aa91d15998dbdb72449819ae82
Chen Oppenhaim <chen.oppenhaim@toluna.com>
2020-05-23 01:44:28 +0300

    Dropping support for react-google-maps

--- public/components/MbGoogleMap.js
+++ public/components/MbGoogleMap.js
@@ -1,30 +1,21 @@
 import React from "react";
-import { compose, withProps } from "recompose";
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+import {Map, Marker} from 'google-maps-react';
+import {useSelector} from "react-redux";
+import {isDesktop} from "../selectors";
 
-// TODO is it ok hard coded?
-const API_KEY = "AIzaSyAgOEl4xMnzBoHSR9CQndSMRafzL3_EeEE";
-const DEFAULT_LOCATION = {
-    latitude: 32.783408,
-    longitude: 35.025506
+const MbGoogleMap = ({location}) => {
+    const desktop = useSelector(isDesktop);
+
+    return (
+        <Map google={window.google} zoom={14}
+             containerStyle={{height: '400px', width: desktop ? '550px' : '100%', position: 'relative'}}
+             initialCenter={location}
+        >
+            <Marker
+                position={location}
+            />
+        </Map>
+    );
 };
-
-const MbGoogleMap = compose(
-    withProps({
-        googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&v=3.exp&libraries=geometry,drawing,places&language=iw`,
-        loadingElement: <div style={{ height: `100%` }} />,
-        containerElement: <div style={{ height: `400px`, width: `600px` }} />,
-        mapElement: <div style={{ height: `100%` }} />,
-    }),
-    withScriptjs,
-    withGoogleMap
-)(({location = DEFAULT_LOCATION}) =>
-    (<GoogleMap
-        defaultZoom={17}
-        defaultCenter={{ lat: parseFloat(location.latitude), lng: parseFloat(location.longitude) }}
-    >
-        <Marker position={{ lat: parseFloat(location.latitude), lng: parseFloat(location.longitude) }}  />
-    </GoogleMap>)
-);
 
 export default MbGoogleMap;
