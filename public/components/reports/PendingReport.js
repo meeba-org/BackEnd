42a4d15714e1934d83ab31f2370d9020672bac1b
Chen Oppenhaim <chen.oppenhaim@toluna.com>
2020-06-16 09:54:53 +0300

    Bug - Pending shifts does not appear in web

--- public/components/reports/PendingReport.js
+++ public/components/reports/PendingReport.js
@@ -1,58 +1,38 @@
 import Tooltip from "@material-ui/core/Tooltip";
 import RefreshIcon from "@material-ui/icons/Refresh";
-import React, {Component} from 'react';
-import FieldArray from "redux-form/es/FieldArray";
-import {DATE_FORMAT, ReportModes} from "../../helpers/utils";
+import React from 'react';
+import {ReportModes} from "../../helpers/utils";
 import '../../styles/DailyReport.scss';
 import MbActionButton from "../MbActionButton";
 import MbActionsControls from "../MbActionsControls";
 import MbCard from "../MbCard";
-import moment from "./DailyReport";
 import ShiftsList from "./ShiftsList";
 
-class PendingReport extends Component {
-    state = {
-
-    };
-
-    onUpdateShift = (shift) => {
-        let value = moment(this.state.currentDay, DATE_FORMAT);
-        this.props.onUpdateShift(shift, value.format('MM'), value.format('YYYY'));
-    };
-
-    render() {
-        const {showShiftDialog, onDeleteShift, postUpdate, onRefresh} = this.props;
-
-        return (
-            <MbCard title={"משמרות ממתינות לאישור"}>
-                    <div styleName="daily-report">
-                        <MbActionsControls>
-                            <Tooltip title="רענן" placement="top">
-                                <MbActionButton
-                                    onClick={onRefresh}
-                                    iconComponent={RefreshIcon}
-                                />
-                            </Tooltip>
-                        </MbActionsControls>
-
-                        <FieldArray
-                            name="shifts"
-                            component={ShiftsList}
-                            onDelete={onDeleteShift}
-                            onUpdate={this.onUpdateShift}
-                            showShiftDialog={showShiftDialog}
-                            showNames={true}
-                            mode={ReportModes.Report}
-                            shouldDisplayNoData={true}
-                            postUpdate={postUpdate}
+const PendingReport = ({shifts, showShiftDialog, onDeleteShift, postUpdate, onRefresh}) => {
+    return (
+        <MbCard title={"משמרות ממתינות לאישור"}>
+            <div styleName="daily-report">
+                <MbActionsControls>
+                    <Tooltip title="רענן" placement="top">
+                        <MbActionButton
+                            onClick={onRefresh}
+                            iconComponent={RefreshIcon}
                         />
-                    </div>
-            </MbCard>
-        );
-    }
-}
+                    </Tooltip>
+                </MbActionsControls>
 
-PendingReport.propTypes = {};
-PendingReport.defaultProps = {};
+                <ShiftsList
+                    shifts={shifts}
+                    onDelete={onDeleteShift}
+                    showShiftDialog={showShiftDialog}
+                    showNames={true}
+                    mode={ReportModes.Report}
+                    shouldDisplayNoData={true}
+                    postUpdate={postUpdate}
+                />
+            </div>
+        </MbCard>
+    );
+};
 
 export default PendingReport;
--- public/components/reports/PendingReportContainer.js
+++ public/components/reports/PendingReportContainer.js
@@ -1,14 +1,10 @@
 import React, {PureComponent} from 'react';
 import {connect} from "react-redux";
-import reduxForm from "redux-form/es/reduxForm";
 import {fetchPendingShifts, showDeleteShiftModal, showEditShiftModal, updateShift} from "../../actions";
 import {getPendingShifts} from "../../selectors";
 import PendingReport from "./PendingReport";
 
 class PendingReportContainer extends PureComponent {
-    state = {
-
-    };
 
     componentDidMount() {
         this.fetchPendingShifts();
@@ -19,20 +15,18 @@ class PendingReportContainer extends PureComponent {
     };
 
     render() {
-        const {handleSubmit, updateShift, deleteShift, showShiftDialog, shifts, isLoading} = this.props;
+        const {updateShift, deleteShift, showShiftDialog, shifts, isLoading} = this.props;
 
         return (
-            <form onSubmit={handleSubmit(() => {})}>
-                <PendingReport
-                    shifts={shifts}
-                    onDeleteShift={deleteShift}
-                    onUpdateShift={updateShift}
-                    showShiftDialog={showShiftDialog}
-                    isLoading={isLoading}
-                    onRefresh={this.fetchPendingShifts}
-                    postUpdate={this.fetchPendingShifts}
-                />
-            </form>
+            <PendingReport
+                shifts={shifts}
+                onDeleteShift={deleteShift}
+                onUpdateShift={updateShift}
+                showShiftDialog={showShiftDialog}
+                isLoading={isLoading}
+                onRefresh={this.fetchPendingShifts}
+                postUpdate={this.fetchPendingShifts}
+            />
         );
     }
 }
@@ -41,26 +35,18 @@ class PendingReportContainer extends PureComponent {
 PendingReportContainer.propTypes = {};
 PendingReportContainer.defaultProps = {};
 
-function mapStateToProps(state) {
-    return {
-        shifts: getPendingShifts(state),
-        initialValues: {
-            shifts: getPendingShifts(state)
-        },
-    };
-}
+const mapStateToProps = state => ({
+    shifts: getPendingShifts(state),
+});
 
-function mapDispatchToProps(dispatch) {
-    return {
-        // hasPendingShifts: () => dispatch( hasPendingShifts()) ,
-        fetchPendingShifts: () => {dispatch( fetchPendingShifts()); },
-        deleteShift: (shift) => dispatch(showDeleteShiftModal(shift, dispatch)),
-        updateShift: (shift, month, year) => dispatch(updateShift(shift, false, month, year)),
-        showShiftDialog: (shift, callBack, postUpdate) => dispatch(showEditShiftModal(shift, callBack, postUpdate)),
-    };
-}
+const mapDispatchToProps = dispatch => ({
+    // hasPendingShifts: () => dispatch( hasPendingShifts()) ,
+    fetchPendingShifts: () => {
+        dispatch(fetchPendingShifts());
+    },
+    deleteShift: (shift) => dispatch(showDeleteShiftModal(shift, dispatch)),
+    updateShift: (shift, month, year) => dispatch(updateShift(shift, false, month, year)),
+    showShiftDialog: (shift, callBack, postUpdate) => dispatch(showEditShiftModal(shift, callBack, postUpdate)),
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(reduxForm({
-    form: 'pendingReportForm',
-    enableReinitialize: true,
-})(PendingReportContainer));
+export default connect(mapStateToProps, mapDispatchToProps)(PendingReportContainer);
