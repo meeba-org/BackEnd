1b0fe6003cc6424a561dd011efe83cc937f635e4
Chen Oppenhaim <chen.oppenhaim@toluna.com>
2020-08-14 08:39:11 +0300

    Fixing bugs

--- public/components/reports/MbInfoLabel.js
+++ public/components/reports/MbInfoLabel.js
@@ -5,7 +5,7 @@ import "../../styles/MbInfoLabel.scss";
 
 const MbInfoLabel = ({label, value, tooltip}) => {
     return (
-        <Tooltip title={tooltip}>
+        <Tooltip title={tooltip} placement="top">
             <Box display="flex" flexDirection="row" styleName="container">
                 <Box styleName="label" fontSize="body1.fontSize">{label}</Box>
                 <Box fontWeight="bold" color={"primary.main"} fontSize="body1.fontSize">{value}</Box>
