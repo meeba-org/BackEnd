5ec6f4418161df2ed4ef38a3e76ebbd8da88336a
Chen Oppenhaim <chen.oppenhaim@toluna.com>
2020-08-31 13:41:29 +0300

    Home refactoring

--- public/components/home/Footer.js
+++ public/components/home/Footer.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import '../../styles/Footer.scss';
+import './styles/Footer.scss';
 import facebookImage from '../../styles/images/facebook.png';
 import Button from "@material-ui/core/Button";
 
