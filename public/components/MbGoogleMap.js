7b7ec0dab7d847f60153bf9b5861843613f0baec
Chen Oppenhaim <chen.oppenhaim@toluna.com>
2020-11-17 19:19:47 +0200

    Playing with marker

--- public/components/MbGoogleMap.js
+++ public/components/MbGoogleMap.js
@@ -13,7 +13,19 @@ const MbGoogleMap = ({location}) => {
         >
             <Marker
                 position={location}
-            />
+                label={{
+                    text: "10:14",
+                    fontWeight: "bold"
+                    // fontFamily: "Arial",
+                    // fontSize: "10px",
+                }}
+                icon={{
+                    // anchor: new window.google.maps.Point(53,53),
+                    labelOrigin: new window.google.maps.Point(53,15),
+                }}
+            >
+                <div>10:00</div>
+            </Marker>
         </Map>
     );
 };
