216d9c979703e9c193a7a9affd5d9a815571a1ff
Chen Oppenhaim <chen.oppenhaim@toluna.com>
2020-11-29 09:47:44 +0200

    Change Marker

--- public/components/MbGoogleMap.js
+++ public/components/MbGoogleMap.js
@@ -8,6 +8,22 @@ import {isDesktop} from "../selectors";
 const MbGoogleMap = ({locations = []}) => {
     const desktop = useSelector(isDesktop);
 
+
+    // Marker settings https://stackoverflow.com/a/23163930/1846993
+    const pinSVGHole = "M12,11.5A2.5,2.5 0 0,1 9.5,9A2.5,2.5 0 0,1 12,6.5A2.5,2.5 0 0,1 14.5,9A2.5,2.5 0 0,1 12,11.5M12,2A7,7 0 0,0 5,9C5,14.25 12,22 12,22C12,22 19,14.25 19,9A7,7 0 0,0 12,2Z";
+    const labelOriginHole = new window.google.maps.Point(12, 25);
+
+    const createMarker = (isOOO = true) => ({
+        path: pinSVGHole,
+        anchor: new window.google.maps.Point(12,17),
+        fillOpacity: 1,
+        fillColor: isOOO ? "red" : "green",
+        strokeWeight: 2,
+        strokeColor: "black",
+        scale: 2,
+        labelOrigin: labelOriginHole
+    });
+    
     const convertLocation = location => ({
         lat: location.latitude,
         lng: location.longitude
@@ -15,7 +31,7 @@ const MbGoogleMap = ({locations = []}) => {
 
     const formatLabel = location => {
         if (!location.date)
-            return null;
+            return undefined;
         
         return moment(location.date).format(TIME_FORMAT);
     };
@@ -26,10 +42,14 @@ const MbGoogleMap = ({locations = []}) => {
              initialCenter={convertLocation(locations[0])}
         >
             {locations?.map((location, index) =>
-                (<Marker key={index}
+                (<Marker
+                    key={index}
                     position={convertLocation(location)}
+                    icon={createMarker()}
                     label={{
                         text: formatLabel(location),
+                        color: "black",
+                        fontSize: "14px",
                         fontWeight: "bold"
                     }}
                 />)
