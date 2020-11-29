ecf2a16482f831578a63c8c209dbca516f044673
Chen Oppenhaim <chen.oppenhaim@toluna.com>
2020-11-29 09:47:53 +0200

    Change Marker

--- public/components/MbGoogleMap.js
+++ public/components/MbGoogleMap.js
@@ -8,7 +8,6 @@ import {isDesktop} from "../selectors";
 const MbGoogleMap = ({locations = []}) => {
     const desktop = useSelector(isDesktop);
 
-
     // Marker settings https://stackoverflow.com/a/23163930/1846993
     const pinSVGHole = "M12,11.5A2.5,2.5 0 0,1 9.5,9A2.5,2.5 0 0,1 12,6.5A2.5,2.5 0 0,1 14.5,9A2.5,2.5 0 0,1 12,11.5M12,2A7,7 0 0,0 5,9C5,14.25 12,22 12,22C12,22 19,14.25 19,9A7,7 0 0,0 12,2Z";
     const labelOriginHole = new window.google.maps.Point(12, 25);
