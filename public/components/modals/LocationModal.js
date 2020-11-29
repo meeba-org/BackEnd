0b600cb4d20990869053e4a42cce1be883aa1cd3
Chen Oppenhaim <chen.oppenhaim@toluna.com>
2020-11-29 09:57:40 +0200

    Supporting backwards in GoogleMap

--- public/components/modals/LocationModal.js
+++ public/components/modals/LocationModal.js
@@ -25,6 +25,19 @@ class LocationModal extends Component {
     handleClose = () => {
         this.props.dispatch(hideLocationModal());
     };
+    
+    getLocations = shift => {
+        if (!shift)
+            return [];
+
+        if (shift.locations && shift.locations.length > 0)
+            return shift.locations;
+
+        if (!shift.location)
+            return [];
+
+        return [shift.location];
+    }
 
     render() {
         let {open, shift, classes} = this.props;
@@ -32,7 +45,8 @@ class LocationModal extends Component {
         if (!shift)
             return null;
         
-        let {locations} = shift;
+        let locations = this.getLocations(shift);
+        
         return (
             <Dialog onClose={this.handleClose} open={open} classes={{paper: classes.dialogContentRoot}}>
                 <DialogContent >
